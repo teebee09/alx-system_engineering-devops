7415b59f6b03fcf1aad65c317e49c83560d388c7
Tijani Oluwatobiloba <tijaniteebee09@gmail.com>
2023-04-27 04:58:02 +0100

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,29 +1,14 @@
-#!/usr/bin/node
-/* prints all characters of a Star Wars movie.
-    - the first argument is the Movie ID.
-    - displays one character name by line.
-*/
-const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (error) console.log(error);
-  else {
-    const characters = JSON.parse(body).characters;
-    characters_dict = {};
-    characters.forEach((character) => {
-      request(character, function (error, response, body) {
-        if (error) console.log(error);
-        else {
-          let id = JSON.parse(body).url;
-          id = id.replace('https://swapi-api.hbtn.io/api/people/', '');
-          id = id.replace('/', '');
-          const name = JSON.parse(body).name;
-          characters_dict[id] = (name);
-        }
-      });
-    });
-    for (let id in characters_dict) {
-        console.log(characters_dict[id]);
-    }
-  }
-});
+#!/usr/bin/python3
+"""Returns to-do list information for a given employee ID."""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
